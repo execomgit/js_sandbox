0467dfe7c9588ff03cd8ddf9f61765d6c521aeeb
Ime <execom.qa@gmail.com>
2018-03-10 10:00:18 +0100

    Finished up with request 02

--- ajax_requests/ajax_animal_02/req_02.js
+++ ajax_requests/ajax_animal_02/req_02.js
@@ -1,18 +1,48 @@
 
+var pageCounter = 1;
 var animalContainer = document.getElementById("animal_info");
 var ajaxButton = document.getElementById("btn");
 
 ajaxButton.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json#");
+  ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-" + pageCounter + ".json");
   ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
-    renderhtml(ourData);
+    renderHTML(ourData);
   };
   ourRequest.send();
+  pageCounter++;
 });
 
 
-function renderhtml(data) {
-  animalContainer.insertAdjacentHTML("beforeend", "test123")
+function renderHTML(data) {
+  var htmlString = "";
+
+  for (i = 0 ; i < data.length ; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+
+    for (e = 0 ; e < data[i].foods.likes.length ; e++) {
+      if (e == 0) {
+        htmlString += data[i].foods.likes[e];
+      } else {
+        htmlString += " and " + data[i].foods.likes[e];
+      };
+
+    };
+
+    htmlString += " and dislikes ";
+    for (e = 0 ; e < data[i].foods.dislikes.length ; e++) {
+      if (e == 0) {
+        htmlString += data[i].foods.dislikes[e];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[e];
+      };
+
+    };
+
+    htmlString += ".</p>";
+
+  };
+
+  animalContainer.insertAdjacentHTML("beforeend", htmlString + "<br>")
 }
